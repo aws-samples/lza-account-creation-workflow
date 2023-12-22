57b14ad4769f012d45e02b4f28a2ff516b24b0b6
Joe Behrens <josepbeh@amazon.com>
2023-12-22 17:02:58 -0600

    Add more unit test for create account helper

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -34,6 +34,22 @@ def mocked_organization(organizations_client):
     organizations_client.tag_resource(
         ResourceId=DEFAULT_ACCOUNT_ID, Tags=[{"Key": "Owner", "Value": "Tester McTest"}]
     )
+    organizations_client.create_organizational_unit(
+        ParentId=organizations_client.list_roots()["Roots"][0]["Id"],
+        Name="ou-test-1",
+    )
+    ou_parent_id = (
+        organizations_client.list_children(
+            ParentId=organizations_client.list_roots()["Roots"][0]["Id"],
+            ChildType="ORGANIZATIONAL_UNIT",
+        )
+    )["Children"][0]["Id"]
+    organizations_client.create_organizational_unit(
+        ParentId=ou_parent_id, Name="ou-test-2"
+    )
+    organizations_client.create_organizational_unit(
+        ParentId=ou_parent_id, Name="ou-test-3"
+    )
 
 
 @pytest.fixture(scope="function")
@@ -51,7 +67,7 @@ def mocked_ses_backend(aws_credentials, monkeypatch):
 
 
 @pytest.fixture
-def stubbed_servicecatalog_client(aws_credentials, request):
+def stubbed_servicecatalog_client_describe_product(aws_credentials, request):
     servicecatalog_client = botocore.session.get_session().create_client(
         "servicecatalog", region_name="us-east-1"
     )
@@ -102,6 +118,86 @@ def stubbed_servicecatalog_client(aws_credentials, request):
 
     stubber.add_response(
         "describe_product", describe_product_response, describe_product_expected_params
+    )
+    stubber.activate()
+    return servicecatalog_client
+
+
+PRODUCT_CREATE_UPDATE_RESPONSE = {
+    "RecordDetail": {
+        "RecordId": "testId1",
+        "ProvisionedProductName": "testProvisionedProduct",
+        "Status": "CREATED",
+        "CreatedTime": datetime(2015, 1, 1),
+        "UpdatedTime": datetime(2015, 1, 1),
+        "ProvisionedProductType": "Test",
+        "RecordType": "Test",
+        "ProvisionedProductId": "testProduct",
+        "ProductId": "testProductId1",
+        "ProvisioningArtifactId": "artifactId1",
+        "PathId": "string",
+        "RecordTags": [
+            {"Key": "tag1", "Value": "value1"},
+            {"Key": "param1", "Value": "value1"},
+        ],
+        "LaunchRoleArn": "aRoleArn",
+    }
+}
+PRODUCT_CREATE_UPDATE_EXPECTED_PARAMS = {
+    "ProductName": "testProduct",
+    "ProvisionedProductName": "testProvisionedProduct",
+    "ProvisioningArtifactId": "testId1",
+    "ProvisioningParameters": [{"Key": "param1", "Value": "value1"}],
+    "Tags": [
+        {"Key": "tag1", "Value": "value1"},
+        {"Key": "SCParameter:param1", "Value": "value1"},
+    ],
+}
+
+
+@pytest.fixture
+def stubbed_servicecatalog_client_provision_product(aws_credentials):
+    """Stubbed service catalog provision new product call
+
+    Args:
+        aws_credentials (_type_): Mocked creds to prevent unintended side effects
+    """
+    servicecatalog_client = botocore.session.get_session().create_client(
+        "servicecatalog", region_name="us-east-1"
+    )
+    stubber = Stubber(servicecatalog_client)
+
+    provision_product_response = PRODUCT_CREATE_UPDATE_RESPONSE
+    provision_product_expected_params = PRODUCT_CREATE_UPDATE_EXPECTED_PARAMS
+
+    stubber.add_response(
+        "provision_product",
+        provision_product_response,
+        provision_product_expected_params,
+    )
+    stubber.activate()
+    return servicecatalog_client
+
+
+@pytest.fixture
+def stubbed_servicecatalog_client_update_provisioned_product(aws_credentials):
+    """Stubbed service catalog update new product call
+
+    Args:
+        aws_credentials (_type_): Mocked creds to prevent unintended side effects
+    """
+    servicecatalog_client = botocore.session.get_session().create_client(
+        "servicecatalog", region_name="us-east-1"
+    )
+    stubber = Stubber(servicecatalog_client)
+
+    update_provisioned_product_response = PRODUCT_CREATE_UPDATE_RESPONSE
+    update_provisioned_product_expected_params = PRODUCT_CREATE_UPDATE_EXPECTED_PARAMS
+
+    stubber.add_response(
+        "update_provisioned_product",
+        update_provisioned_product_response,
+        update_provisioned_product_expected_params,
     )
     stubber.activate()
     return servicecatalog_client
